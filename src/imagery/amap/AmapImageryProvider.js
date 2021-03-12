9e50d4608e7feaf97d77d6cdb4d4118b2ebbbde4
kerbores <kerbores@gmail.com>
2021-03-12 08:38:06 +0800

    :sparkles: 增加高德路网标注图层
    Signed-off-by: kerbores <kerbores@gmail.com>

--- src/imagery/amap/AmapImageryProvider.js
+++ src/imagery/amap/AmapImageryProvider.js
@@ -11,9 +11,12 @@ const IMG_URL =
 const ELEC_URL =
   'https://webrd{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}'
 
+const LOAD_MARK_URL =
+  'https://webrd{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}'
+
 class AmapImageryProvider extends Cesium.UrlTemplateImageryProvider {
   constructor(options = {}) {
-    options['url'] = options.style === 'img' ? IMG_URL : ELEC_URL
+    options['url'] = options.style === 'img' ? IMG_URL options.style === 'load'? LOAD_MARK_URL : ELEC_URL
     if (!options.subdomains || !options.subdomains.length) {
       options['subdomains'] = ['01', '02', '03', '04']
     }
